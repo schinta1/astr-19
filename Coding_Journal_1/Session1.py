60aa3c5b4fd1c501526c3d71960d595b9d066a4f
Sree Chinta <schinta@ucsc.edu>
2023-09-29 16:36:10 -0700

    finished session 1 and 2 from coding journal #1

---
+++ Coding_Journal_1/Session1.py
@@ -0,0 +1,10 @@
+
+def main():
+    name = "Sree Chinta"
+    pronouns = "He/Him"
+    movie = "It"
+    food = "ramen"
+    print (f"\n Name: {name}, \n Pronouns: {pronouns}, \n My favorite movie is {movie}. \n My favorite food is {food}. \n ")
+    
+if __name__ == "__main__":
+    main()
---
+++ Coding_Journal_1/Session2.py
@@ -0,0 +1,21 @@
+def main():
+    
+    f1 = 3.32
+    f2 = 3.45
+    sum = f1 + f2
+    print(f"\nSum of float 1: {f1} and float 2: {f2} is {sum} and it's type is {type(sum)}.")
+    
+    i1 = 10 
+    i2 = 12
+    difference = i1 - i2
+    print(f"The difference of integer 1: {f1} and integer 2: {f2} is {difference} and it's type is {type(difference)}.")
+    
+    i3 = 10
+    f3 = 2.34
+    product = i3 * f3
+    print(f"The product of integer: {i3} and float {f3} is {product} and it's type is {type(product)}.")
+    
+    
+
+if __name__ == "__main__":
+    main()
