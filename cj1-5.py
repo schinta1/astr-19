0197f82c4d8c969d31ba19a093d6b861ee9c48aa
Sree Chinta <schinta@ucsc.edu>
2023-10-18 15:48:14 -0700

    finished cj1-5

--- cj1-5.py
+++ cj1-5.py
@@ -1,8 +1,18 @@
+import numpy as np
+import astropy
+from astropy.table import Table
+from astropy.io import ascii
+from astropy.io import fits
+
+
 def main():
-    
-    return
-    
-    
-    
+    x = np.linspace(0, 2 * np.pi, 1000)
+    y = np.sin(x)
+    data = Table([x, y], names=["x", "sin(x)"])
+    ascii.write(data, "table.txt", format="commented_header")
+    data_in = ascii.read("table.txt")
+    print(data_in)
+
+
 if __name__ == "__main__":
     main()
