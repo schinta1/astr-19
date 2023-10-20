b82ce4d704fae2fff9b482faea91178a7bb46935
Sree Chinta <schinta@ucsc.edu>
2023-10-19 19:57:35 -0700

    finished

--- cj1-3.py
+++ cj1-3.py
@@ -1,8 +1,9 @@
 def main(x):
-    return (x**3 + 8)
-    
+    return x**3 + 8
+
+
 if __name__ == "__main__":
     result = main(9)
-    print (result)
+    print(result)
     if result > 27:
         print("YAY!")
